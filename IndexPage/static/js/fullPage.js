189a2e463c088d5b7bc0b7257ae22fbc72cf7e46
growplus <growpluscro@gmail.com>
2016-05-12 20:42:09 +0200

    Prva strana

---
+++ IndexPage/static/js/fullPage.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+     $('#fullpage').fullpage({
+        sectionsColor: ['#ffffff', '#ff9900', '#009900', 'whitesmoke', '#000'],
+        scrollingSpeed: 750
+    });
+});
